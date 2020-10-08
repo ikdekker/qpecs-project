66922176a8d9e5f43630ca9c6d4f9edae75141a5
Nick Dekker <nick@freshcoders.nl>
2020-10-08 14:44:54 +0000

    Update with json support

--- anova.py
+++ anova.py
@@ -34,15 +34,45 @@ sign_table = [[list(factors.values())[i][0] if x == -1 else list(factors.values(
 # in our 2^k factorial (with n factors): 2^n
 # we have 5 now, so 2^5=32
 
-#data = json_parse_to_dict()
-# json_normalized = [np.r_[list(val.values())] for val in json['model']]
-# data = []
-# for config_string in json_normalized:
-#   setting = x.split(",")
-#   setting_info = setting.split("-")
-#   data_key = "{}|{}|{}|{}|{}".format(setting_info['GBRAM'],setting_info['...'])
-#   data[data_key](setting_info['])
-# return data
+import json
+
+with open('data.json') as f:
+  data = json.load(f)
+
+def json_parse_to_dict(data, model):
+
+  json_normalized = np.r_[list(data[model].values())].flatten()
+  data = {}
+
+  # first do a loop to average out the replications
+#  replication_values = {}
+#  for config_string in json_normalized:
+#    setting = config_string.split(",")
+#    setting_info = {}
+#    for s in setting:
+#      setting_info[s.split("-")[1].strip()] = s.split("-")[0].strip()
+#    data_key = "{}|{}|{}|{}|{}".format(setting_info['GBRAM'],setting_info['Cores'],setting_info['batchSize'],setting_info['learningRate'],setting_info['learningRateDecay'])
+#    if not data_key in replication_values:
+#      replication_values[data_key] = []
+#    replication_values[data_key].append(setting_info['responsetime'])
+
+#  averages = {}
+#  for k, v in replication_values.items():
+#    averages[k] = np.average(list(map(float, v)))
+
+  for config_string in json_normalized:
+    setting = config_string.split(",")
+    setting_info = {}
+    for s in setting:
+      setting_info[s.split("-")[1].strip()] = s.split("-")[0].strip()
+    data_key = "{}|{}|{}|{}|{}".format(setting_info['GBRAM'],setting_info['Cores'],setting_info['batchSize'],setting_info['learningRate'],setting_info['learningRateDecay'])
+    if not data_key in data:
+      data[data_key] = []
+    data[data_key].append(setting_info['responsetime'])
+
+  return data
+data = json_parse_to_dict(data,'bi-rnn')
+
 
 data_names = {}
 for sign_row in sign_table:
@@ -54,18 +84,25 @@ for sign_row in sign_table:
 
 #print(data_names)
 
-#response_times = []
-#for config in sign_table:
-#  response_times = data[
-#
+response_times = []
 
-response_times = range(1,33) # needs to be grabbed from a loop over factors that look up in the json data
+reps = 1
 
+for r in range(0, reps):
+  for config in sign_table:
+    conf_str = "|".join(list(map(str, config)))
+    if conf_str in data and len(data[conf_str]) >= (r+1):
+      response_times.append(float(data[conf_str][r]))
+    else:
+      response_times.append(8888888) #maybe use average for missing cases?
+
+print(response_times)
+#response_times = range(1,33) # needs to be grabbed from a loop over factors that look up in the json data
 
 d = {'Response': response_times}
 d2 = {**data_names, **d}
 
 df = pd.DataFrame(data=d2)
 # Gettin summary statistics
-#print(rp.summary_cont(df.groupby( list(factors.keys()) ))['Response'])
-print(rp.summary_cont(df.groupby( 'ram' ))['Response'])
+print(rp.summary_cont(df.groupby( list(factors.keys()) ))['Response'])
+#print(rp.summary_cont(df.groupby( 'ram' ))['Response'])
